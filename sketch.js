8215748a8066b6377905e51a16ea74d5c37c7dfb
AlexanderVengel <Next16273@edu.nextkbh.dk>
2025-04-10 14:29:51 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+let  posVec, velVec, nemo, haj, sharkposvec
+function preload(){
+    Akvarium = loadImage('/Assets/Aquarium.png')
+}
+
+
+function setup() 
+{
+	createCanvas(1520, 770);
+    imageMode(CORNERS)
+    
+    posVec = new Vector(4,1)
+    velVec = new Vector(2,2.5)
+    sharkposvec = new Vector(2,1)
+    sharkvelvec = new Vector(3,4)
+    dariusvelvec= new Vector(1,2)
+    nemo = new Fish(posVec,velVec)
+    haj = new Shark(sharkposvec,sharkvelvec)
+    darius = new Fish(posVec,dariusvelvec)
+}
+
+function draw()
+{
+    image(Akvarium,0,0,1520,770)
+    nemo.show("red")
+    nemo.update()
+    haj.show("Darkblue")
+    haj.update()
+    darius.show("red")
+    darius.update()
+}
